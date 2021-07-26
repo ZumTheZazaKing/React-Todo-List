0225fa376caa74fc1376a96c117ad106560941bd
ZumTheZazaKing <zahidi85543@gmail.com>
2021-07-26 23:17:40 +0800

    Creation of the app with it's most simple feature of adding tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,69 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
 
-function App(){
-  return <div>
-    <h1>Hello!</h1>
+function AddTaskForm(props){
+
+  let [value, setValue] = useState("");
+
+  function handleChange(e){
+
+    setValue(e.target.value);
+
+  }
+
+  return <div className="form">
+
+    <form onSubmit={e => {props.handleSubmit(value); setValue(""); e.preventDefault()}}>
+
+      <input type="text" value={value} onChange={handleChange} required/>
+      <input type="submit" value="+"/>
+
+    </form>
+
   </div>
+
+}
+
+
+function TaskList(props){
+
+  let arr = props.data;
+  let listItems = arr.map(val => <li>{val}</li>)
+
+  return <div className="list">
+
+    <ul>{listItems}</ul>
+
+  </div>
+
+}
+
+
+
+function App(){
+
+  let [tasks, setTasks] = useState([]);
+
+
+  function changeTasks(addition){
+
+    setTasks([...tasks, addition])
+
+  }
+
+  return <div className="container">
+
+    <h1>ToDo List</h1>
+
+    <AddTaskForm handleSubmit={changeTasks}/>
+    <br />
+    <TaskList data={tasks}/>
+
+  </div>
+
 }
 
 
